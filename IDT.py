94e0af5d4061171d54235ec521529581ffd9921b
Paweł <pwkucia@gmail.com>
2018-08-16 19:14:49 +0200

    Something is counting... Hopefully :D

--- IDT.py
+++ IDT.py
@@ -8,31 +8,41 @@ def calculateIdtAlgorithm(pointsList):
     windowList = []
     coordXList = []
     coordYList = []
-    try:
-        while(i < len(pointsList) - 1):
-            if (pointsList[i].Type == 'SS'):
-                i += 1
-                continue
+    countPoints = len(pointsList)
+    while(i < countPoints - 1):
+        if (pointsList[i].Type == 'SS'):
+            i += 1
+            continue
+        currTime = int(pointsList[i].TimeStamp)
+        while currTime - timeStart <= constants.WINDOW_TIME_THRESHOLD:
+            windowList.append(pointsList[i])
+            i += 1
+            if i >= countPoints:
+                break
             currTime = int(pointsList[i].TimeStamp)
-            while currTime - timeStart <= constants.WINDOW_TIME_THRESHOLD:
-                windowList.append(pointsList[i])
-                i += 1
-                currTime = int(pointsList[i].TimeStamp)
-            if len(windowList) > 1:
-                Dispersion = (max(maxX.CoordX for maxX in windowList) - min(minX.CoordX for minX in windowList)) + (max(maxY.CoordY for maxY in windowList) - min(minY.CoordY for minY in windowList))
+        if i >= countPoints:
+            break
+        if len(windowList) > 1:
+            Dispersion = (max(maxX.CoordX for maxX in windowList) - min(minX.CoordX for minX in windowList)) + (max(maxY.CoordY for maxY in windowList) - min(minY.CoordY for minY in windowList))
+            print('Dispersion: %.2f' % Dispersion)
+        while len(windowList) > 1:
             if Dispersion <= constants.DISPERSION_THRESHOLD and len(windowList) > 1:
+                print('wchodze')
                 while (Dispersion < constants.DISPERSION_THRESHOLD):
+                    print('calcdisp')
                     windowList.append(pointsList[i])
                     i += 1
+                    if i >= countPoints:
+                        break
                     Dispersion = (max(maxX.CoordX for maxX in windowList) - min(minX.CoordX for minX in windowList)) + (max(maxY.CoordY for maxY in windowList) - min(minY.CoordY for minY in windowList))
+                if i >= countPoints:
+                    print('brk')
+                    break
                 coordXList.append(sum(sumX.CoordX for sumX in windowList) / len(windowList))
                 coordYList.append(sum(sumY.CoordY for sumY in windowList) / len(windowList))
             else:
                 windowList.pop(0)
-            if i <= len(pointsList) - 1:
-                timeStart = int(pointsList[i].TimeStamp)
-        end = time.process_time()
-        return coordXList, coordYList, '%.3f' % (end - start), len(coordXList)
-
-    except Exception as e:
-        print('exception %s' % e)
+        if i <= countPoints - 1:
+            timeStart = int(pointsList[i].TimeStamp)
+    end = time.process_time()
+    return coordXList, coordYList, '%.3f' % (end - start), len(coordXList)
