94670d648fb755c4a7bbdec9ae7f5f8d41c7eda9
Paweł Kucia <pwkucia@gmail.com>
2019-11-06 07:29:47 +0100

    end

--- data.ps1
+++ data.ps1
@@ -1,10 +1,10 @@
 Set-Location $PSScriptRoot
-Get-ChildItem 'result\ml-prop-0-5\ML*' -Filter *.csv | Select-Object -ExpandProperty FullName | Import-Csv | Export-Csv .\merged\ml-prop\merged1.csv -NoTypeInformation
-Get-ChildItem 'data\' -Filter *.cal | Select-Object Name,FullName | Export-Csv .\merged\ml-prop\merged2.csv -NoTypeInformation
-Select-String -Path 'result\ml-prop-0-5\*.log' -Pattern ".*coordX, coordY, fixationsForPoint, timealgorithm, ite, measurementFixations, saccadeCount..*calculateMl." |
+Get-ChildItem 'result\idt-disp-0-05\I-DT*' -Filter *.csv | Select-Object -ExpandProperty FullName | Import-Csv | Export-Csv .\merged\idt-disp\merged1.csv -NoTypeInformation
+Get-ChildItem 'data\' -Filter *.cal | Select-Object Name,FullName | Export-Csv .\merged\idt-disp\merged2.csv -NoTypeInformation
+Select-String -Path 'result\idt-disp-0-05\*.log' -Pattern ".*coordX, coordY, timealgorithm, fixationsForPoint, fixations, saccade..*calculateId." |
 Select-Object @{
     Name = "Line"
     Expression = {($_.Line -split '\s+')[4]}
 } |
 #ForEach-Object {Line.Split(" ")} |
-Export-Csv .\merged\ml-prop\merged3.csv -NoTypeInformation
+Export-Csv .\merged\idt-disp\merged3.csv -NoTypeInformation
